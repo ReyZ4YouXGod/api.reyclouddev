36fd2662b0a5e85f08ec35b5434cf410e37da877
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-05-31 21:49:54 +0700

    Update mediafire.js

--- download/mediafire.js
+++ download/mediafire.js
@@ -3,23 +3,16 @@ const { mediafire } = require("../../utils/mediafire");
 
 module.exports = async (req, res) => {
   try {
-    // 🔥 GLOBAL CORS
     if (cors(req, res)) return;
 
     if (req.method !== "GET") {
-      return res.status(405).json({
-        status: false,
-        message: "GET only"
-      });
+      return res.status(405).json({ status: false });
     }
 
     const { url } = req.query;
 
     if (!url) {
-      return res.status(400).json({
-        status: false,
-        message: "url wajib"
-      });
+      return res.status(400).json({ status: false, message: "url wajib" });
     }
 
     const result = await mediafire(url);
