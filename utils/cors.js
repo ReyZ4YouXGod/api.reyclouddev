7c989a22096a82de1ef2547f067d0a558648c558
ReyZ4YouXGod <raihanananda767@gmail.com>
2026-05-31 21:43:20 +0700

    Create cors.js

---
+++ utils/cors.js
@@ -0,0 +1,14 @@
+function cors(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return true; // stop execution
+  }
+
+  return false; // lanjut ke handler utama
+}
+
+module.exports = cors;
